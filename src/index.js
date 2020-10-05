b56491c198747425e1069a23c6d0187861634366
Ekaterina Bulatova <katerinavitalevna@ya.ru>
2020-10-05 11:54:24 +0400

    solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  const str = String(n);
+  if (str[0] === '-') {
+    return Number(str.slice(1).split('').reverse().join(''));
+  }
+  return Number(str.split('').reverse().join(''));
 }
